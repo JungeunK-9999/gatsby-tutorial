b6a1fe8fb3c2628626c63d1292b9325b76cf1396
Jung <jungeun.lak@gmail.com>
2020-08-07 12:44:38 +0900

    5. Source Plugins and Rendering Queried Data

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,13 +9,19 @@ module.exports = {
   siteMetadata: {
     title: `Title from siteMetadata`,
   },
-  plugins: [
-    `gatsby-plugin-emotion`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
+  plugins: [{
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `src`,
+      path: `${__dirname}/src/`,
     },
+  },
+    `gatsby-plugin-emotion`,
+  {
+    resolve: `gatsby-plugin-typography`,
+    options: {
+      pathToConfigModule: `src/utils/typography`,
+    },
+  },
   ],
 }
