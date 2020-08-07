3a63b3537459ad545a8805e8edbceef463898b57
Jung <jungeun.lak@gmail.com>
2020-08-07 11:09:29 +0900

    3. Creating Nested Layout Components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
 import React from "react"
-import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
-      <Link to="/contact/">Contact</Link>
-      <h1>그레이트 코멧</h1>
-      <p>comming soon!</p>
-      <img src="https://image.genie.co.kr/Y/IMAGE/IMG_MUZICAT/IV2/Genie_Magazine/8006/Mgz_Sub_IMG_20200207121923.jpg" alt="" />
-      <button class="primary-button">Click me</button>
-    </div>
+    <Layout>
+      <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+      <p>
+        What do I like to do? Lots of course but definitely enjoy building
+        websites.
+    </p>
+    </Layout>
   );
   // return React.createElement("div", null, "Hello world!")
 }
